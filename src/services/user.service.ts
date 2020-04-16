a67e5201b90454b0c3829934ecb368e2e0d8d915
SimonHuet <huetsimon35@gmail.com>
2020-04-16 09:34:44 +0200

    feat(): Adding User Service

---
+++ src/services/user.service.ts
@@ -0,0 +1,21 @@
+import {inject, Provider} from '@loopback/core';
+import {getService} from '@loopback/service-proxy';
+import {ApiUserDataSource} from '../datasources';
+
+export interface User {
+  // this is where you define the Node.js methods that will be
+  // mapped to REST/SOAP/gRPC operations as stated in the datasource
+  // json file.
+}
+
+export class UserProvider implements Provider<User> {
+  constructor(
+    // api_user must match the name property in the datasource json file
+    @inject('datasources.api_user')
+    protected dataSource: ApiUserDataSource = new ApiUserDataSource(),
+  ) {}
+
+  value(): Promise<User> {
+    return getService(this.dataSource);
+  }
+}
